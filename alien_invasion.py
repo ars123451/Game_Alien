414caeb8aca38ad2e8a3dc18deb9eae972465987
ARS <aaaaa@mail.ru>
2022-11-02 01:27:47 +0300

    1) Начало проекта + создание корабля (слишком большой)

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,16 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
-        self.screen = pygame.display.set_mode((1366,768))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (115, 195, 215)
+        self.ship = Ship(self)
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
@@ -15,7 +18,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 # Отображение последнего прорисованного экрана
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
+                self.ship.blitme()
                 pygame.display.flip()
 if __name__ == '__main__':
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Settings():
     '''Класс для хранения всех настроек игры Alien Invasion.'''
     def __init__(self):
         # Параметры экрана
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 1366
+        self.screen_height = 768
+        self.bg_color = (255, 200, 230)
