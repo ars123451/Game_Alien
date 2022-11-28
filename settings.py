f28382939bf07b6e6b117fdd9024b8b3edec6c4f
ARS <aaaaa@mail.ru>
2022-11-28 04:04:13 +0300

    движение корабля, стрельба

--- settings.py
+++ settings.py
@@ -5,7 +5,17 @@ class Settings():
         # Параметры экрана
         self.screen_width = 1366
         self.screen_height = 750
-        a = random.randint(0,255)
-        b = random.randint(0, 255)
-        c = random.randint(0, 255)
+        a = random.randint(100,255)
+        b = random.randint(100, 255)
+        c = random.randint(100, 255)
         self.bg_color = (b, a, c)
+        '''Настройки корабля'''
+        self.ship_speed = 1
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 5
+        self.bullet_height = 10
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+
--- ship.py
+++ ship.py
@@ -1,10 +1,11 @@
 import pygame
-
+import settings
 class Ship():
     '''Класс для управления кораблем.'''
     def __init__(self, ai_game):
         '''Инициализирует корабль и задает его начальную позицию.'''
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружает изображение корабля и получает прямоугольник.
@@ -12,14 +13,16 @@ class Ship():
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
         '''Флаг перемещения'''
         self.moving_right = False
         self.moving_left = False
     def update(self):
-        if self.moving_right == True:
-            self.rect.x += 1
-        if self.moving_left == True:
-            self.rect.x -= 1
+        if self.moving_right == True and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left == True and self.rect.left > self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
     def blitme(self):
         '''Рисует корабль в текущей позиции'''
         self.screen.blit(self.image, self.rect)
